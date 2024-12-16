839ab4922deb36a1fa2e5c3698a43d9b87e8494e
Raghu K <raghuk.vit@gmail.com>
2024-12-16 07:58:47 +0530

    RoboShop

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 sed -i -e "s/LICENSE_KEY/${NEW_RELIC_LICENSE_KEY}/" newrelic.ini
+export PATH=${PATH}:/app/.local/bin/
 NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program uwsgi --ini payment.ini
 
